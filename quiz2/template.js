5d5cd6e9f2f3f0b8e2a7ce47f0fb5166a0b2800f
tjy86 <thomasjyang@gmail.com>
2012-10-26 09:39:34 -0400

    quiz 2

--- quiz2/template.js
+++ quiz2/template.js
@@ -1,3 +1,14 @@
 $(function(){
-
+	$('#make').click(make);
 });
+
+function make () {
+	a = $('#number').val();
+	a = parseFloat(a);
+	for (i=0;i<a;i++){
+		div = $('<div>');
+		div.addClass('box');
+		div.text(i+1);
+		$('#heaven').prepend(div);
+	}
+}

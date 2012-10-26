6c6ec4ad61c40d440a7a2e387e07248f2ea8cb3c
tjy86 <thomasjyang@gmail.com>
2012-10-26 10:01:45 -0400

    quiz 3

--- quiz3/template.js
+++ quiz3/template.js
@@ -1,3 +1,24 @@
 $(function(){
-
+	$('#make').click(make);
+	$('#heaven').on({mouseenter:hover,mouseleave:out},'.box');
 });
+
+function make () {
+	a = $('#colors').val();
+	a = a.split(' ');
+	for (i=0;i<a.length;i++){
+		div = $('<div>');
+		div.addClass('box');
+		div.css('background',a[i]);
+		$('#heaven').append(div);
+	}
+}
+
+function hover () {
+	z = $(this).css('background');
+	$('#hell').css('background',z);
+}
+
+function out () {
+	$('#hell').css('background','');
+}
